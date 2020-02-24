3ea4b836004c090723ac22b20157b29a2df6cb62
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-02-24 18:46:41 +0800

    more tests

--- src/LazyWAVFiles.jl
+++ src/LazyWAVFiles.jl
@@ -1,61 +1,69 @@
 module LazyWAVFiles
-    using WAV, LazyArrays
-    export LazyWAVFile, DistributedWAVFile, path
-    struct LazyWAVFile{T,N,S<:Tuple} <: AbstractArray{T,N}
-        path::String
-        size::S
+using WAV, LazyArrays
+export LazyWAVFile, DistributedWAVFile, path
+
+struct LazyWAVFile{T,N,S<:Tuple} <: AbstractArray{T,N}
+    path::String
+    size::S
+end
+function LazyWAVFile(path)
+    r = wavread(path, format="native", subrange=1)[1]
+    s = wavread(path, format="size")
+    T = eltype(r)
+    dim = s[2]
+    if dim == 1
+        s = (s[1],)
     end
-    function LazyWAVFile(path)
-        r = wavread(path, format="native", subrange=1)[1]
-        s = wavread(path, format="size")
-        T = eltype(r)
-        N = length(s[2])
-        if N == 1
-            s = (s[1],)
-        end
-        LazyWAVFile{T,N,typeof(s)}(path, s)
+    LazyWAVFile{T,dim,typeof(s)}(path, s)
+end
+Base.size(f::LazyWAVFile) = f.size
+Base.size(f::LazyWAVFile{T,N},i) where {T,N} = i > N ? 1 : f.size[i]
+Base.length(f::LazyWAVFile) = prod(f.size)
+path(f::LazyWAVFile) = f.path
+
+Base.getindex(f::LazyWAVFile{T,N}, i::Number) where {T,N} = wavread(f.path, format="native", subrange=i:i)[1][1]::T
+
+Base.getindex(f::LazyWAVFile{T,N}, i,j) where {T,N} = wavread(f.path, format="native", subrange=i:i)[1][j]
+Base.getindex(f::LazyWAVFile{T,1}, i::AbstractRange) where {T} = vec(wavread(f.path, format="native", subrange=i)[1])::Array{T,1}
+Base.getindex(f::LazyWAVFile{T,2}, i::AbstractRange) where {T} = wavread(f.path, format="native", subrange=i)[1]::Array{T,2}
+
+Base.getindex(f::LazyWAVFile{T,1}, ::Colon) where {T} = vec(wavread(f.path, format="native")[1])::Array{T,1}
+Base.getindex(f::LazyWAVFile{T,2}, ::Colon) where {T} = wavread(f.path, format="native")[1]::Array{T,2}
+
+Base.eltype(f::LazyWAVFile{T}) where T = T
+Base.ndims(f::LazyWAVFile{T,N}) where {T,N} = N
+Base.show(io::IO, f::LazyWAVFile{T,N,S}) where {T,N,S} = println(io, "LazyWAV{$T, $N, $(f.size)}: ", f.path)
+
+
+struct DistributedWAVFile{T,N,L} <: AbstractArray{T,N}
+    files::Vector{LazyWAVFile{T,N}}
+    lazyarray::L
+end
+function DistributedWAVFile(folder::String)
+    files = filter(readdir(folder)) do file
+        lowercase(file[end-2:end]) == "wav"
     end
-    Base.size(f::LazyWAVFile) = f.size
-    Base.size(f::LazyWAVFile{T,N},i) where {T,N} = i > N ? 1 : f.size[i]
-    Base.length(f::LazyWAVFile) = prod(f.size)
-    path(f::LazyWAVFile) = f.path
-
-    Base.getindex(f::LazyWAVFile{T,N}, i::Number) where {T,N} = wavread(f.path, format="native", subrange=i:i)[1][1]::T
-    Base.getindex(f::LazyWAVFile{T,1}, i::AbstractRange) where {T} = vec(wavread(f.path, format="native", subrange=i)[1])::Array{T,1}
-    Base.getindex(f::LazyWAVFile{T,2}, i::AbstractRange) where {T} = wavread(f.path, format="native", subrange=i)[1]::Array{T,2}
-
-    Base.getindex(f::LazyWAVFile{T,1}, ::Colon) where {T} = vec(wavread(f.path, format="native")[1])::Array{T,1}
-    Base.getindex(f::LazyWAVFile{T,2}, ::Colon) where {T} = wavread(f.path, format="native")[1]::Array{T,2}
-
-    Base.eltype(f::LazyWAVFile{T}) where T = T
-    Base.ndims(f::LazyWAVFile{T,N}) where {T,N} = N
-    Base.show(io::IO, f::LazyWAVFile{T,N,S}) where {T,N,S} = println(io, "LazyWAV{$T, $N, $(f.size)}: ", f.path)
-
-
-    struct DistributedWAVFile{T,N,L} <: AbstractArray{T,N}
-        files::Vector{LazyWAVFile{T,N}}
-        lazyarray::L
+    files = sort(files)
+    files = LazyWAVFile.(joinpath.(Ref(folder), files))
+    DistributedWAVFile(files)
+end
+function DistributedWAVFile(files)
+    lazyarray = Vcat(files...)
+    try
+        return DistributedWAVFile{eltype(files[1]), ndims(files[1]), typeof(lazyarray)}(files, lazyarray)
+    catch e
+        @error "Creating distributed WAV file failed. This can happen if the wav-files in the folder have different number of channels."
+        rethrow(e)
     end
-    function DistributedWAVFile(folder::String)
-        files = filter(readdir(folder)) do file
-            lowercase(file[end-2:end]) == "wav"
-        end
-        files = sort(files)
-        files = LazyWAVFile.(joinpath.(Ref(folder), files))
-        DistributedWAVFile(files)
-    end
-    function DistributedWAVFile(files)
-        lazyarray = Vcat(files...)
-        DistributedWAVFile{eltype(files[1]), ndims(files[1]), typeof(lazyarray)}(files, lazyarray)
-    end
-    Base.length(f::DistributedWAVFile) = sum(length, f.files)
-    Base.size(f::DistributedWAVFile{T,N}) where {T,N} = ntuple(i->sum(x->size(x,i), f.files), N)
+end
+Base.length(f::DistributedWAVFile) = sum(length, f.files)
+Base.size(f::DistributedWAVFile{T,N}) where {T,N} = ntuple(i->sum(x->size(x,i), f.files), N)
 
-    Base.show(io::IO, f::DistributedWAVFile{T,N}) where {T,N} = println(io, "DistributedWAVFile{$T, $N} with $(length(f.files)) files")
+Base.show(io::IO, f::DistributedWAVFile{T,N}) where {T,N} = println(io, "DistributedWAVFile{$T, $N} with $(length(f.files)) files")
 
-    Base.getindex(df::DistributedWAVFile, i...) = getindex(df.lazyarray, i...)
+Base.getindex(df::DistributedWAVFile, i...) = getindex(df.lazyarray, i...)
 
-    Base.vcat(dfs::DistributedWAVFile...) = DistributedWAVFile(reduce(vcat, getfield.(dfs, :files)))
+Base.vcat(dfs::DistributedWAVFile...) = DistributedWAVFile(reduce(vcat, getfield.(dfs, :files)))
 
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,72 @@
 using Test, LazyWAVFiles, WAV
 
-d = mktempdir()
-a,b = randn(Float32,10), randn(Float32,10)
-WAV.wavwrite(a, joinpath(d,"f1.wav"), Fs=8000)
-WAV.wavwrite(b, joinpath(d,"f2.wav"), Fs=8000)
 
-df = DistributedWAVFile(d)
-@test df[1] == a[1]
-@test df[1:2] == a[1:2]
-@test df[1:10] == a
-@test df[:] == [a;b]
-@test df[9:11] == [a[9:end];b[1]]
-@test df[[1,3,5]] == a[[1,3,5]]
-@test df[[1,3,5,12]] == [a[[1,3,5]];b[2]]
+@testset "LazyWAVFiles" begin
+    @info "Testing LazyWAVFiles"
+
+    d = mktempdir()
+    a,b = randn(Float32,10), randn(Float32,10)
+    WAV.wavwrite(a, joinpath(d,"f1.wav"), Fs=8000)
+    WAV.wavwrite(b, joinpath(d,"f2.wav"), Fs=8000)
+    p1 = joinpath(d,"f1.wav")
 
 
-@test size(df) == (20,)
-@test length(df) == 20
-@test length(df.files[1]) == 10
-@test length([df; df]) == 40
-@test_nowarn display(df)
-@test_nowarn display(df.files[1])
+    @testset "One file, one channel" begin
+        @info "Testing One file, one channel"
 
-@test ndims(df.files[1]) == 1
+        lf = LazyWAVFile(p1)
+        @test size(lf) == (10,)
+        @test path(lf) == p1
+        @test lf[1] == a[1]
+        @test lf[1:2] == a[1:2]
+        @test lf[1:10] == a
+
+    end
+
+
+    @testset "Distributed file" begin
+        @info "Testing Distributed file"
+
+        df = DistributedWAVFile(d)
+        @test df[1] == a[1]
+        @test df[1:2] == a[1:2]
+        @test df[1:10] == a
+        @test df[:] == [a;b]
+        @test df[9:11] == [a[9:end];b[1]]
+        @test df[[1,3,5]] == a[[1,3,5]]
+        @test df[[1,3,5,12]] == [a[[1,3,5]];b[2]]
+
+
+        @test size(df) == (20,)
+        @test length(df) == 20
+        @test length(df.files[1]) == 10
+        @test length([df; df]) == 40
+        @test_nowarn display(df)
+        @test_nowarn display(df.files[1])
+
+        @test ndims(df.files[1]) == 1
+    end
+
+    @testset "Multiple channels" begin
+        @info "Testing Multiple channels"
+
+        WAV.wavwrite([a b], joinpath(d,"f3.wav"), Fs=8000)
+        p3 = joinpath(d,"f3.wav")
+        lf = LazyWAVFile(p3)
+        @test size(lf) == (10,2)
+        @test path(lf) == p3
+        @test lf[1] == a[1]
+        @test lf[1,2] == b[1]
+        @test lf[1:10] == [a b]
+    end
+
+    @testset "Misc" begin
+        @info "Testing Misc"
+
+        @info "The following error message is intentional"
+        @test_throws MethodError DistributedWAVFile(d)
+
+    end
+
+
+end
